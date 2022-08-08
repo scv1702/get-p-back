4a41d2c4ce8e638d65729fa2f2368f3b2ee4f31a
Changyu Shin <scv1702@naver.com>
2022-08-09 00:13:08 +0900

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors();
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
